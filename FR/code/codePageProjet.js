ec2b5140ab5c1ff789feab2e41b2a2c16547e197
simlefebvre <59534624+simlefebvre@users.noreply.github.com>
2022-07-19 22:55:03 +0200

    changement de traitement pour éviter les erreur 404 en cas d'espace

--- FR/code/codePageProjet.js
+++ FR/code/codePageProjet.js
@@ -6,15 +6,25 @@ document.getElementById('drapeau').addEventListener('click', function(e) {
 
 
 function initialisation(nomProjet){
-    document.title = nomProjet;
+    let titre = nomProjet.replaceAll("%20", " ",);
+            titre = titre.replaceAll("%C3%A9", "é");
+            titre = titre.replaceAll("%C3%A8", "è");
+            titre = titre.replaceAll("%C3%A0", "à");
+            titre = titre.replaceAll("%C3%A7", "ç");
+            titre = titre.replaceAll("%C3%B4", "ô");
+            titre = titre.replaceAll("%C3%B9", "ù");
+            titre = titre.replaceAll("%27", "'");
+            titre = titre.replaceAll("%C3%A2", "â");
     listElt = document.getElementsByClassName('titre');
     for (let elt of listElt) {
         if(elt.id == "titrePage"){
-            elt.innerHTML = nomProjet;
+            
+
+            elt.innerHTML = titre;
             elt.id = "MenuHome";
         }
         else{
-            elt.innerHTML = "<strong>" +nomProjet+"</strong>";
+            elt.innerHTML = "<strong>" +titre+"</strong>";
         }
     }
     
@@ -36,7 +46,7 @@ function initialisation(nomProjet){
 }
 
 const queryString = location.search.substring(1);
-if(!queryString.match(/^([a-zA-Z 0-9]+)$/)){
+if(!queryString.match(/^([a-zA-Z 0-9%]+)$/)){
     window.open('../404.html','_self');
 }else{
     initialisation(queryString);
